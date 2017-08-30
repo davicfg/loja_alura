3982fe9d3c23ec3fdccf49fde2c6c2bcfe668d85
davi carvalho <davi.carvalho@redemaisvoce.com.br>
2017-08-30 08:07:58 -0300

    nao eraa pra subir esse arquivo, asm ele ta subindo mesmo assim.

--- conecta.php
+++ conecta.php
@@ -1,2 +1,3 @@
 <?php
-$conexao = mysqli_connect("mysql.hostinger.com.br", "u548680052_loja", "lojaloja", "u548680052_loja");
+// $conexao = mysqli_connect("mysql.hostinger.com.br", "u548680052_loja", "lojaloja", "u548680052_loja");
+$conexao = mysqli_connect("mysql.hostinger.com.br", "u263542819_davi", "davi123456", "	u263542819_alura");
