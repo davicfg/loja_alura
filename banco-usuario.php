394c2cd31d7f0f8aa3bf033215978c1b96607f9f
Davi Carvalho <davicfg@hotmail.com>
2017-09-07 22:45:55 -0300

    prevenção deo injeção de sql no login.

--- banco-usuario.php
+++ banco-usuario.php
@@ -2,10 +2,10 @@
 	function buscaUsuario($conexao,$email,$senha){
 		$s_md5 = md5($senha);
 		$query = "select * from usuarios where email='{$email}' and senha ='{$s_md5}'";
+        $email = mysqli_real_escape_string($conexao, $email);
 		//echo $query; exit;
 		$rs= mysqli_query($conexao, $query);
 		$usuario = mysqli_fetch_assoc($rs);
 		return $usuario;
 	}
-	
 ?>
