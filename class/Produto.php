339c62a6cac865328539a5e033d13baf4759eab4
davicfg <davicfg@hotmail.com>
2017-09-22 13:45:58 -0300

    metodo preco com desocnto

--- class/Produto.php
+++ class/Produto.php
@@ -9,6 +9,13 @@
     public $descricao;
     public $categoria;
     public $usado;
+    private $descontoPadrao = 0.1;
+
+    function precoComDesconto($valor = $this->descontoPadrao){
+      $this->preco -= $this->preco $valor;
+      return $this->preco;
+
+    }
 
   }
 
