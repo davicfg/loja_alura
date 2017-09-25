1fe5e08429f357afae30272911e413184c12134f
Davi Carvalho <davicfg@hotmail.com>
2017-09-24 23:19:44 -0300

    removendo id do cosrutor

--- class/Produto.php
+++ class/Produto.php
@@ -1,7 +1,6 @@
 <?php
 
-  class Produto
-  {
+class Produto {
 
     private $id;
     private $nome;
@@ -10,73 +9,58 @@
     private $categoria;
     private $usado;
 
-    function precoComDesconto($desconto = 0.1){
-      if($desconto > 0 && $desconto <=0.5){
-          $this->preco = $this->preco - ($this->preco * $desconto);;
-      }
-      return $this->preco;
+    function __construct($nome, $preco, $descricao, Categoria $categoria, $usado) {
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->descricao = $descricao;
+        $this->categoria = $categoria;
+        $this->usado = $usado;
     }
 
-    public function getId()
-      {
-          return $this->id;
-      }
+    public function getId() {
+        return $this->id;
+    }
 
-      public function setId($id)
-      {
-          $this->id = $id;
-      }
+    public function setId($id) {
+        $this->id = $id;
+    }
 
-      public function getNome()
-      {
-          return $this->nome;
-      }
+    public function getNome() {
+        return $this->nome;
+    }
 
-      public function setNome($nome)
-      {
-          $this->nome = $nome;
-      }
+    public function getPreco() {
+        return $this->preco;
+    }
 
-      public function getPreco()
-      {
-          return $this->preco;
-      }
+    public function getDescricao() {
+        return $this->descricao;
+    }
 
-      public function setPreco($preco)
-      {
-          $this->preco = $preco;
-      }
+    public function getCategoria() {
+        return $this->categoria;
+    }
 
-      public function getDescricao()
-      {
-          return $this->descricao;
-      }
+    public function isUsado() {
+        return $this->usado;
+    }
 
-      public function setDescricao($descricao)
-      {
-          $this->descricao = $descricao;
-      }
+    public function setUsado($usado) {
+        $this->usado = $usado;
+    }
 
-      public function getCategoria()
-      {
-          return $this->categoria;
-      }
+    public function precoComDesconto($valor = 0.1) {
 
-      public function setCategoria($categoria)
-      {
-          $this->categoria = $categoria;
-      }
+        if ($valor > 0 && $valor <= 0.5) {
+            $this->preco -= $this->preco * $valor;
+        }
 
-      public function isUsado()
-      {
-          return $this->usado;
-      }
+        return $this->preco;
+    }
 
-      public function setUsado($usado)
-      {
-          $this->usado = $usado;
-      }
-
-  }
+    function __toString() {
+        return $this->nome.": R$ ".$this->preco;
+    }
+}
 
 ?>
