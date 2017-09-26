050aee54b3b9542d810230a965a4aca865cbf209
davicfg <davicfg@hotmail.com>
2017-09-26 08:21:16 -0300

    criando classe livro qque extende de produto

---
+++ class/Livro.php
@@ -0,0 +1,22 @@
+<?php
+
+  class Livro extends Produto {
+
+    private isbn;
+
+    function __construct($isbn)
+    {
+      $this->isbn = $isn;
+    }
+  }
+
+  public function getIsbn() {
+    return $this->isbn;
+  }
+
+  public function setIsbn($isbn) {
+    $this->isbn = $isbn;
+  }
+
+
+ ?>
